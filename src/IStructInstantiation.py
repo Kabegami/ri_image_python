1cd2bb5b901c89a6a6f7a767bcda7e0c0d5a2330
Nethanel Dabi <luff789@gmail.com>
2019-01-16 23:49:30 +0100

    Conversion squelette code java to python

---
+++ src/IStructInstantiation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 23:33:21 2019
+
+@author: dabi-
+"""
+
+class IStructInstantiation:
+    
+    def __init__(self):
+        pass
+    
+    def psi(self, x, y):
+        pass
+    
+    def delta(self, y1, y2):
+        pass
+    
+    def enumerateY(self):
+        pass
+    
+        
+    
---
+++ src/STrainingSample.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 23:25:05 2019
+
+@author: dabi-
+"""
+
+class STrainingSample():
+    
+    def __init__(self):
+        self.input = None;
+        self.output = None;
+        
+    def STrainingSample(self, x, y):
+        self.input=x;
+        self.output=y;
+        
+
+        
+
+        
